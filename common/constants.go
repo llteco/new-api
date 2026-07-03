db2e827a8d3676417ae4fcc21752f38e4eaec424
Wenyi Tang <wenyitang@outlook.com>
2026-07-03 22:55:31 +0800

    [dev] add ChannelStatusTempDisabled constant
    Signed-off-by: AGENTS

--- common/constants.go
+++ common/constants.go
@@ -267,6 +267,7 @@ const (
 	ChannelStatusEnabled          = 1 // don't use 0, 0 is the default value!
 	ChannelStatusManuallyDisabled = 2 // also don't use 0
 	ChannelStatusAutoDisabled     = 3
+	ChannelStatusTempDisabled     = 4 // limit cooldown
 )
 
 const (
